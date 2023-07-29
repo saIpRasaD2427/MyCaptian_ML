0100d01549410ddc4cd624bb41a2cf7297828ac8
saIpRasaD2427 <102215545+saIpRasaD2427@users.noreply.github.com>
2023-07-29 11:54:02 +0530

    Create set_operations.py

---
+++ set_operations.py
@@ -0,0 +1,6 @@
+A = {0,2,4,6,8,10,12,14,16};
+B = {1,2,3,4,5,7,9,11,13,15,17};
+print("Union :", A | B)
+print("Intersection :", A & B)
+print("Difference :", A - B)
+print("Symmetric difference :", A ^ B)
